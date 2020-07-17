35e465eff9b77e4467e2bbc24868ddd0d1ae2d88
Philip <jhasondeleon@gmail.com>
2020-07-17 18:06:12 -0400

    edit or delete cell layout added

---
+++ app/src/main/java/com/example/contact_philip_c0778584_android/ShowUser.java
@@ -0,0 +1,14 @@
+package com.example.contact_philip_c0778584_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_user);
+    }
+}
